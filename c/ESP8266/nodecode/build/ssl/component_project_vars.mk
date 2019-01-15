20de9e293b9be6451fe2e061897723c22060a586
mam1 <mam1@cornell.edu>
2019-01-15 09:30:41 -0700

    screwing with esp8266 development environment

--- c/ESP8266/nodecode/build/ssl/component_project_vars.mk
+++ c/ESP8266/nodecode/build/ssl/component_project_vars.mk
@@ -1,7 +1,7 @@
 # Automatically generated build file. Do not edit.
 COMPONENT_INCLUDES += $(IDF_PATH)/components/ssl/wolfssl/include $(IDF_PATH)/components/ssl/wolfssl/wolfssl $(IDF_PATH)/components/ssl/wolfssl/wolfssl/wolfssl
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/ssl -L $(IDF_PATH)/components/ssl/wolfssl/lib -lwolfssl
-COMPONENT_LINKER_DEPS += $(IDF_PATH)/components/ssl/wolfssl/lib/libwolfssl.a
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/ssl -L $(IDF_PATH)/components/ssl/wolfssl/lib -lwolfssl_debug
+COMPONENT_LINKER_DEPS += $(IDF_PATH)/components/ssl/wolfssl/lib/libwolfssl_debug.a
 COMPONENT_SUBMODULES += 
 COMPONENT_LIBRARIES += ssl
 component-ssl-build: 
